8822e942d8da0a476062b6af01fd598809f0f2ce
dongeon110 <dongeon110@gmail.com>
2022-06-10 13:37:58 +0900

    meals to 3nutrient

--- machine129/meals.py
+++ machine129/meals.py
@@ -159,7 +159,7 @@ class meals():
                 );")
             self.conn.commit()
     
-    def dis_food(self, id, conf=0.5, recent = 1):
+    def dis_food(self, id, conf=0.5, recent = 1, str=False):
         # id에 맞는 최근 질병탐지 기록 가져오기 (result_row)
         # recent = 최근 기준 첫번째 1 / 두번째 2 ...
         cur = self.conn.cursor(pymysql.cursors.DictCursor)
@@ -212,7 +212,40 @@ class meals():
         for bad_food_str in bad_food_list:
             bad_food_list_int.append(int(bad_food_str))
         
+        if str:
+            mcdic = {0:'잡곡밥', 1:'배추김치', 2:'된장찌개',
+            3:'김치찌개', 4:'멸치볶음', 5:'시금치나물', 
+            6:'가지나물', 7:'고사리나물', 8:'미역국',
+            9:'김밥', 10:'불고기', 11:'애호박볶음',
+            12:'무생채', 13:'잡채', 14:'무국', 
+            15:'고등어구이', 16:'쌀밥', 17:'계란후라이',
+            18:'계란말이', 19:'제육볶음',20:'시래기국', 
+            21:'땅콩조림'}
+
+            bad_food_list_str =[]
+            for bad_food_class in bad_food_list_int:
+                bad_food_str = mcdic[int(bad_food_class)]
+                bad_food_list_str.append(bad_food_str)
+            
+            return bad_food_list_str
         return bad_food_list_int
+
+    def meals_to_3nutrient(self, id, recent = 1):
+        cur = self.conn.cursor(pymysql.cursors.DictCursor)
+        try:
+            cur.execute(f'SELECT * FROM meals WHERE user_id = {id} ORDER BY date_time DESC;')
+            dic_result = cur.fetchall()
+            result = pd.DataFrame(dic_result)
+        except:
+            return {}
+        
+        calorie = result.head(recent)['calorie_kcal'][0]
+        carbo = result.head(recent)['carbohydrate_g'][0]
+        protein = result.head(recent)['protein_g'][0]
+        fat = result.head(recent)['fat_g'][0]
+
+        dict_result = {'calorie':calorie, 'carbo':carbo, 'protein':protein, 'fat':fat}
+        return dict_result
 
 
 
